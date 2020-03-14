21b7d5fb371acd6d1f8dbbd5fe0280fdb27327c9
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2020-03-14 07:46:24 +0100

    Refactor JsonDumper

--- lib/json_dumper.ex
+++ lib/json_dumper.ex
@@ -1,11 +1,41 @@
 defmodule TzExtra.JsonDumper do
   @moduledoc false
 
-  def dump(fun, filename) do
-    json = Jason.encode!(fun.())
+  def dump_countries(filename \\ "countries.json") do
+    json =
+      TzExtra.countries()
+      |> Enum.map(&camelize_map_keys(&1))
+      |> Jason.encode!()
 
     file_path = Path.join(:code.priv_dir(:tz_extra), filename)
 
     File.write!(file_path, json, [:write])
+  end
+
+  def dump_countries_time_zones(filename \\ "countries_time_zones.json") do
+    json =
+      TzExtra.countries_time_zones()
+      |> Enum.map(&camelize_map_keys(&1))
+      |> Jason.encode!()
+
+    file_path = Path.join(:code.priv_dir(:tz_extra), filename)
+
+    File.write!(file_path, json, [:write])
+  end
+
+  defp camelize_map_keys(map) do
+    for {key, val} <- map, into: %{}, do: {camelize(to_string(key)), val}
+  end
+
+  defp camelize(string) do
+    String.split(string, "_")
+    |> Enum.map(&String.capitalize(&1))
+    |> Enum.join()
+    |> uncapitalize()
+  end
+
+  defp uncapitalize(string) do
+    {first, rest} = String.split_at(string, 1)
+    String.downcase(first) <> rest
   end
 end
