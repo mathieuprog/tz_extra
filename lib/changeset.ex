729ba2cb0f196ca299d658ac3c4f276b1821733f
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2024-08-20 14:22:21 +0800

    Fix

--- lib/changeset.ex
+++ lib/changeset.ex
@@ -45,9 +45,9 @@ if Code.ensure_loaded?(Ecto.Changeset) do
       end)
     end
 
-    def validate_country_time_zone(%Ecto.Changeset{} = changeset, fields, opts \\ []) do
-      field_names = Keyword.keys(fields)
-      field_types = Keyword.values(fields)
+    def validate_country_time_zone(%Ecto.Changeset{} = changeset, fields_meta, opts \\ []) do
+      field_names = Keyword.keys(fields_meta)
+      field_types = Keyword.values(fields_meta)
 
       unless [:country_code, :time_zone_id] -- field_types == [] do
         raise "second argument must be a keyword list where the values are :country_code and :time_zone_id"
@@ -58,7 +58,7 @@ if Code.ensure_loaded?(Ecto.Changeset) do
 
         {country_code, time_zone_id} =
           cond do
-            Keyword.fetch!(fields, field_name_1) == :country_code ->
+            Keyword.fetch!(fields_meta, field_name_1) == :country_code ->
               {field_value_1, field_value_2}
 
             true ->
